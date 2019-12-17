dde87d5798e890dc2029a2593e7b0f8329d3e363
Jowel <rjowel@gmail.com>
2019-12-17 15:51:25 -0500

    added buttons in filter

--- open-fridge/src/component/Filter.jsx
+++ open-fridge/src/component/Filter.jsx
@@ -4,7 +4,31 @@ import './styles/Filter.css'
 const Filter = () => {
     return (
         <div className="filter">
-            <input type="range" min="5" max="120" value="50" />
+            <h3>Time</h3>
+            <input type="range" min="5" max="120" className="slider" />
+            <div className="temperature">
+                <h3>Temperature</h3>
+                <button>Hot</button>
+                <button>Cold</button>
+            </div>
+            <div className="flavor">
+                <h3>Flavor</h3>
+                <div className="attributes">
+                <button>Sweet</button>
+                <button>Savory</button>
+                <button>Tangy</button>
+                <button>Dry</button>
+                <button>Saucy</button>
+                <button>Soupy</button>
+                <button>Plain</button>
+                <button>Mild</button>
+                <button>Spicy</button>
+                <button>Soft</button>
+                <button>Chewy</button>
+                <button>Crunchy</button>
+                </div>
+
+            </div>
         </div>
     )
 }
