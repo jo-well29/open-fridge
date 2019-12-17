4b6f9b84d27f42d45680a69b1f90f8f43cd9c069
Jowel <rjowel@gmail.com>
2019-12-17 13:49:40 -0500

    styled the header

--- open-fridge/src/component/Header.jsx
+++ open-fridge/src/component/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import '../styles/Header.css'
+import './styles/Header.css'
+import './images/fridgeLogo.png'
 
 
-export const Header = () => {
+const Header = () => {
     return (
-        <>
-        </>
+        <div className="header">
+            <div className="logo"><img src="https://i.imgur.com/ESkARC2.png" alt="" /></div>
+            <h1>Open Fridge</h1>
+            <div className="hamburger-menu"><img src="https://i.imgur.com/igYHBRK.png" alt="" /></div>
+        </div>
     )
 }
+
+export default Header
