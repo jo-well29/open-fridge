3d12973b616000095d22b39a352cd1e91d6f6b08
jeremyrrose <jeremy.r.rose@gmail.com>
2019-12-17 21:43:16 -0500

    filter input reflected in both app state and visual display

--- open-fridge/src/component/Filter.jsx
+++ open-fridge/src/component/Filter.jsx
@@ -5,28 +5,25 @@ import FilterButton from './shared/FilterButton'
 import { flavors } from '../config'
 
 const Filter = (props) => {
-    console.log(flavors)
-    const flavorButtons = flavors.map(flavor => (<FilterButton name={flavor} onClick={(e) => props.onSelect(e, 'filterFlavor')}>{flavor}</FilterButton>));
+    const flavorButtons = flavors.map(flavor => (<FilterButton name={flavor} className={props.formData.filterFlavor.includes(flavor) ? 'selected' : ''} onClick={(e) => props.onSelect(e, 'filterFlavor')}>{flavor}</FilterButton>));
     return (
         <>
             <ToggleHeader onClick={props.onClick} expand={props.expand} sectionName="filter" label="Filter" color="magenta" />
             <div className={`content ${props.expand === 'filter' ? 'expand' : 'contract'}`}>
-                <div className="filter">
-                    <h3>Time</h3>
-                    <input name="filterTime" type="range" step="15" value={props.formData.filterTime} min="15" max="60" className="slider" onChange={props.onChange} />
-                    <p>{props.formData.filterTime} minutes</p>
-                    <div className="temperature">
-                        <h3>Temperature</h3>
-                        <FilterButton name="Hot" onClick={props.tempToggle} />
-                        <FilterButton name="Cold" onClick={props.tempToggle} />
+                <h3>Time</h3>
+                <input name="filterTime" type="range" step="15" value={props.formData.filterTime} min="15" max="60" className="slider" onChange={props.onChange} />
+                <p>{props.formData.filterTime} minutes</p>
+                <div className="temperature">
+                    <h3>Temperature</h3>
+                    <FilterButton name="Hot" className={props.formData.filterTemp === 'Hot' ? 'selected' : ''} onClick={(e) => props.onSelect(e, 'filterFlavor')}onClick={props.tempToggle} />
+                    <FilterButton name="Cold" className={props.formData.filterTemp === 'Cold' ? 'selected' : ''} onClick={(e) => props.onSelect(e, 'filterFlavor')}onClick={props.tempToggle} />
+                </div>
+                <div className="flavor">
+                    <h3>Flavor</h3>
+                    <div className="attributes">
+                        { flavorButtons }
                     </div>
-                    <div className="flavor">
-                        <h3>Flavor</h3>
-                        <div className="attributes">
-                            { flavorButtons }
-                        </div>
 
-                    </div>
                 </div>
             </div>
         </>
--- open-fridge/src/component/shared/FilterButton.js
+++ open-fridge/src/component/shared/FilterButton.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import '../styles/FilterButton.css'
 
 export const FilterButton = ({ onClick, className, list, color, name, variant }) => (
 	<button
 		name={name}
 		onClick={(e) => onClick(e, list)}
-		className={`button ${className} ${variant} ${color}`}>
+		className={`filterButton ${className} ${variant} ${color}`}>
 		{name}
 	</button>
 )
