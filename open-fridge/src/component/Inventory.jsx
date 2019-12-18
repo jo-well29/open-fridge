eebad28c2a163c86d314e9b133f2635004c4d50a
Andrew J Kim <andrewk745@gmail.com>
2019-12-18 10:12:21 -0500

    inventory

--- open-fridge/src/component/Inventory.jsx
+++ open-fridge/src/component/Inventory.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './styles/Inventory.css'
 import ToggleHeader from './shared/ToggleHeader'
+import MultipleItems from './shared/Carousel'
 
 export const Inventory = (props) => {
     return (
         <>
             <ToggleHeader onClick={props.onClick} expand={props.expand} sectionName="inventory" label="What's in your fridge?" color="cocoa" />
             <div className={`content ${props.expand === 'inventory' ? 'expand' : 'contract'}` }>
-                The content
+                <MultipleItems/>
             </div>
         </>
     )
