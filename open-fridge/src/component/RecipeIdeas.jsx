2eff6c7b33e9f31e6e80aca4fa14209ce8d020df
Jowel <rjowel@gmail.com>
2019-12-19 16:34:26 -0500

    styling

--- open-fridge/src/component/RecipeIdeas.jsx
+++ open-fridge/src/component/RecipeIdeas.jsx
@@ -5,20 +5,20 @@ import { RecipeTile } from './shared/RecipeTile'
 import RecipeCard from './shared/RecipeCard'
 import ToggleHeader from './shared/ToggleHeader'
 import { recipeIndexer } from '../config'
-   
+
 export default class RecipeIdeas extends React.Component {
     constructor(props) {
-        super(props) 
-            this.state = {
-                modalClick: false,
-                modalRecipe: {
-                    image: '',
-                    name: '',
-                    ingredients: [],
-                    instructions: [],
-                    description: '',
-                    time: 0
-                }
+        super(props)
+        this.state = {
+            modalClick: false,
+            modalRecipe: {
+                image: '',
+                name: '',
+                ingredients: [],
+                instructions: [],
+                description: '',
+                time: 0
+            }
         }
     }
 
@@ -31,47 +31,52 @@ export default class RecipeIdeas extends React.Component {
     modalClick = (e, recipe) => {
         if (recipe) {
             this.setState({
-                modalClick:!this.state.modalClick,
+                modalClick: !this.state.modalClick,
                 modalRecipe: recipe
             })
         } else {
             this.setState({
-                modalClick:!this.state.modalClick
+                modalClick: !this.state.modalClick
             })
         }
     }
-        
-    render() {
 
+    render() {
+        const style = this.state.modalClicked &&
+            {
+                backgroundColor: 'black'
+            }
         const recipeTiles = this.results().map((recipe, i) => <RecipeCard key={i} image_url={recipe.image} name={recipe.name} cooktime={recipe.time} handleClick={this.modalClick} recipe={recipe} />)
         console.log(this.state.modalClick)
 
-        const modal = 
-        <div className="modal">
-            <div className="exit-button" onClick={this.modalClick}>X</div>
-            <img src={this.state.modalRecipe.image}/> 
-            <div>{this.state.modalRecipe.name.toUpperCase()}</div>
-            <div>Ingredients: {Object.keys(this.state.modalRecipe.ingredients).join(', ')}</div>
-            <div>Cook time: {this.state.modalRecipe.time}</div>
-            <div>Description: {this.state.modalRecipe.description}</div>
+        const modal =
+            <div className="modal">
+                <div className="exit-button" onClick={this.modalClick}>X</div>
+                <img src={this.state.modalRecipe.image} />
+                <div>{this.state.modalRecipe.name.toUpperCase()}</div>
+                <div>Ingredients: {Object.keys(this.state.modalRecipe.ingredients).join(', ')}</div>
+                <div>Cook time: {this.state.modalRecipe.time}</div>
+                <div>Description: {this.state.modalRecipe.description}</div>
 
-            <button>LET'S MAKE THIS</button>
-        </div>
-            
+                <button>LET'S MAKE THIS</button>
+            </div>
+
         const renderModal = this.state.modalClick ? modal : false
 
         return (
             <>
+
                 <ToggleHeader onClick={this.props.onClick} expand={this.props.expand} sectionName="recipes" label="Recipe ideas" color="mint" />
                 <div className={`content ${this.props.expand === 'recipes' ? 'expand' : 'contract'}`}>
                     <div className='RecipeIdeas-container'>
                         {recipeTiles}
-                        <RecipeCard image_url='https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&w=1000&q=80' name='SUPER-DUPER-CASSEROLE' cooktime='15' 
-                        handleClick={this.modalClick}/>
+                        <RecipeCard image_url='https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&w=1000&q=80' name='SUPER-DUPER-CASSEROLE' cooktime='15'
+                            handleClick={this.modalClick} />
                         {/* <RecipeTile image_url='https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&w=1000&q=80' name='SUPER-DUPER-CASSEROLE' cooktime='15' /> */}
                         {renderModal}
                     </div>
                 </div>
+
             </>
         )
     }
